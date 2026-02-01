b08aaaa87176830790bfd7d321c0759c7495dd1e
Mnogo_workal <Work@MacBook-Air-Vladislav.local>
2026-02-01 19:43:17 +0300

    refactoring buttons through functions

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 const firstInput = document.getElementById('firstInput')
 const secondInput = document.getElementById('secondInput')
 const resultValue = document.getElementById('resultValue')
-const addition = document.getElementById('addition')
-const minus = document.getElementById('minus')
-const multiplication = document.getElementById('multiplication')
-const division = document.getElementById('division')
 
-addition.addEventListener('click', () => {
-    resultValue.innerHTML = Number(firstInput.value) + Number(secondInput.value)
-});
 
-minus.addEventListener('click', () => {
-    resultValue.innerHTML = Number(firstInput.value) - Number(secondInput.value)
-});
+function colorValue(first, second) {
+    let result = Number(first) 
+    if (result > 0) {
+        resultValue.innerHTML = result        
+    }
+}
+function addition () {
+    resultValue.innerHTML = Number(firstInput.value) + Number(secondInput.value)  
+};
 
-multiplication.addEventListener('click', () => {
-    resultValue.innerHTML = Number(firstInput.value) * Number(secondInput.value)
-});
+function minus () {
+    resultValue.innerHTML = Number(firstInput.value) - Number(secondInput.value)  
+};
 
-division.addEventListener('click', () => {
-    resultValue.innerHTML = Number(firstInput.value) / Number(secondInput.value)
-});
+function multiple () {
+    resultValue.innerHTML = Number(firstInput.value) * Number(secondInput.value)  
+};
+
+function division () {
+    resultValue.innerHTML = Number(firstInput.value) / Number(secondInput.value)  
+};
+// division.addEventListener('click', () => {
+//     resultValue.innerHTML = Number(firstInput.value) / Number(secondInput.value)
+// });
