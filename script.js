ca4c73ce7da06c8df75a1a947be83ba49f88d50b
Mnogo_workal <Work@MacBook-Air-Vladislav.local>
2026-02-01 20:29:15 +0300

    add function for display colors

--- script.js
+++ script.js
@@ -3,27 +3,27 @@ const secondInput = document.getElementById('secondInput')
 const resultValue = document.getElementById('resultValue')
 
 
-function colorValue(first, second) {
-    let result = Number(first) 
-    if (result > 0) {
-        resultValue.innerHTML = result        
+function colorValue(result) {
+    resultValue.classList.remove('positive', 'negative');
+
+    if (Number(result) > 0) {
+        resultValue.classList.toggle('positive')         
+    } else if(Number(result) < 0) {
+        resultValue.classList.toggle('negative')
     }
 }
 function addition () {
-    resultValue.innerHTML = Number(firstInput.value) + Number(secondInput.value)  
+    colorValue(resultValue.innerHTML = Number(firstInput.value) + Number(secondInput.value))
 };
 
 function minus () {
-    resultValue.innerHTML = Number(firstInput.value) - Number(secondInput.value)  
+    colorValue(resultValue.innerHTML = Number(firstInput.value) - Number(secondInput.value))
 };
 
 function multiple () {
-    resultValue.innerHTML = Number(firstInput.value) * Number(secondInput.value)  
+    colorValue(resultValue.innerHTML = Number(firstInput.value) * Number(secondInput.value))
 };
 
 function division () {
-    resultValue.innerHTML = Number(firstInput.value) / Number(secondInput.value)  
+    colorValue(resultValue.innerHTML = Number(firstInput.value) / Number(secondInput.value)) 
 };
-// division.addEventListener('click', () => {
-//     resultValue.innerHTML = Number(firstInput.value) / Number(secondInput.value)
-// });
